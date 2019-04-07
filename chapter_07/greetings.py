8e6ce4d7078b7513c86762f2e19db9b7b9fb6561
Valentine Shemyako <v.shemyako@gmail.com>
2019-04-07 16:01:25 +0300

    Examined possibility to specify default parameters' values, typing info

--- chapter_07/greetings.py
+++ chapter_07/greetings.py
@@ -1,6 +1,21 @@
-def greet_user():
+def greet_user(name: str = "Stranger") -> None:
     """Prints greeting message to standard output"""
-    print("Hello, stranger!")
+    message_template = "Hello, {}!"
+    greeting_message = message_template.format(name)
+    print(greeting_message)
 
 
-greet_user()
+greet_user("Johnnie Walker")
+
+
+def offer_food(name: str, food: str) -> None:
+    greet_user(name)
+    food_offer_template = "Here's your tasty {}! Enjoy!"
+    food_offer_message = food_offer_template.format(food)
+    print(food_offer_message)
+
+
+offer_food("Clare", "Potatoes")
+
+# basic usage of keyword arguments
+offer_food(food="Tomato Soup", name="Jan")

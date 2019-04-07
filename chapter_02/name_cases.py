b80b2e6667e500b6f916a57ecc060d23afcfad22
Valentine Shemyako <v.shemyako@gmail.com>
2019-04-07 09:57:43 +0300

    Examined case changing functionality

---
+++ chapter_02/name_cases.py
@@ -0,0 +1,8 @@
+name = "valentinE"
+upper_cased_name = name.upper()
+lower_cased_name = name.lower()
+titled_name = name.title()
+
+print(upper_cased_name)
+print(lower_cased_name)
+print(titled_name)

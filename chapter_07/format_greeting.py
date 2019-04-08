e9d0550b48888f5f810e8cd63895765a20707f7b
Valentine Shemyako <v.shemyako@gmail.com>
2019-04-08 09:07:51 +0300

    Polished greeting application

--- chapter_07/format_greeting.py
+++ chapter_07/format_greeting.py
@@ -6,25 +6,32 @@ def greet(name: str = "Stranger") -> None:
     print("Hello, {}".format(name))
 
 
-def getQuitString() -> str:
+def say_goodbye() -> None:
+    print("Goodbye!")
+
+
+def get_quit_string() -> str:
     return "q"
 
 
-def isQuitString(input: str) -> bool:
-    return getQuitString() == input
+def is_quit_string(input: str) -> bool:
+    return get_quit_string() == input
 
 
 while True:
     print("Please enter your name!")
-    print("In case you don't want to proceed, enter '{}'}.".format(getQuitString()))
+    print("In case you don't want to proceed, enter '{}'.".format(get_quit_string()))
 
     first_name = input("First name: ")
-    if isQuitString(first_name):
+    if is_quit_string(first_name):
+        say_goodbye()
         break
 
     last_name = input("Last name: ")
-    if isQuitString(last_name):
+    if is_quit_string(last_name):
+        say_goodbye()
         break
 
     composed_name = compose_titled_name(first_name, last_name)
     greet(composed_name)
+    print()

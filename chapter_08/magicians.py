2cc6d73bd9ab4ab10d31ef02d722277cbac16b90
Valentine Shemyako <v.shemyako@gmail.com>
2019-04-08 22:18:52 +0300

    Completed 'magicians' exercise

---
+++ chapter_08/magicians.py
@@ -0,0 +1,7 @@
+def show_magicians(magicians: [str]) -> None:
+    for magician in magicians:
+        print(magician)
+
+
+magicians = ["Merlin", "Gandalf", "Dumbledore"]
+show_magicians(magicians)

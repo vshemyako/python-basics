42e4e0857e6c2e64925e517a8e53339ed9eb8f0a
Valentine Shemyako <v.shemyako@gmail.com>
2019-04-08 22:27:43 +0300

    Added 'make great' function, to modify magician's name

--- chapter_08/magicians.py
+++ chapter_08/magicians.py
@@ -1,7 +1,14 @@
+def make_great(magicians: [str]) -> [str]:
+    great_template = "{} the Great"
+    for index, value in enumerate(magicians):
+        magicians[index] = great_template.format(value)
+
+
 def show_magicians(magicians: [str]) -> None:
     for magician in magicians:
         print(magician)
 
 
 magicians = ["Merlin", "Gandalf", "Dumbledore"]
+make_great(magicians)
 show_magicians(magicians)
